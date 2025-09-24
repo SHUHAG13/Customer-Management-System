bdb10b3cfa941f6c967cbf6250ebd743edb9c748
shuhag <shuhag200113@gmail.com>
2025-09-25 00:35:21 +0600

    feat(backend): add Customer model, AppDbContext, and EF Core configuration

--- Customer Management System/Data/AppDbContext.cs
+++ Customer Management System/Data/AppDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace Customer_Management_System.Data
+﻿using Customer_Management_System.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Customer_Management_System.Data
 {
-    public class AppDbContext
+    public class AppDbContext:DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        
+        public DbSet<Customer> Customers { get; set; }
     }
 }
--- Customer Management System/Program.cs
+++ Customer Management System/Program.cs
@@ -1,4 +1,7 @@
 
+using Customer_Management_System.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Customer_Management_System
 {
     public class Program
@@ -13,6 +16,8 @@ namespace Customer_Management_System
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 

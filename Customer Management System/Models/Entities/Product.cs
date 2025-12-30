1b523f9e4dd90af75685bd6e6384ff53b2836dd6
SHUHAG13 <shuhag200113@gmail.com>
2025-12-30 22:04:08 +0600

    update project for learning source tree

---
+++ Customer Management System/Models/Entities/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace Customer_Management_System.Models.Entities
+{
+    public class Product
+    {
+        public int MyProperty { get; set; }
+    }
+}
---
+++ Customer Management System/Models/Entities/category.cs
@@ -0,0 +1,7 @@
+﻿namespace Customer_Management_System.Models.Entities
+{
+    public class category
+    {
+        public int MyProperty { get; set; }
+    }
+}
